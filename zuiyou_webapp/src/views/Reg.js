fc0273333892d1d281c68eae1bdac769e57d3b85
liuyiliang07 <851260830@qq.com>
2020-09-24 21:33:08 +0800

    提交reg界面ui

--- zuiyou_webapp/src/views/Reg.js
+++ zuiyou_webapp/src/views/Reg.js
@@ -1,22 +1,39 @@
 import React from 'react'
-import {Button, InputItem} from 'antd-mobile'
-import {createForm} from 'rc-form' 
-class Reg extends React.Component{
-  render(){
+import { Button, InputItem, Icon } from 'antd-mobile'
+import { WechatOutlined, QqOutlined, WeiboOutlined } from '@ant-design/icons'
+import { createForm } from 'rc-form'
+import "../scss/reg.scss"
+class Reg extends React.Component {
+  render() {
     const { getFieldProps } = this.props.form;
-    return(
-        <div><InputItem
-        {...getFieldProps('phone')}
-        type="phone"
-        placeholder="填写本人手机号"
-      >手机号码</InputItem>
-      <InputItem
-        {...getFieldProps('password')}
-        type="password"
-        placeholder="6~12位数字、字母"
-      >密码</InputItem>
-      <Button type="primary" style={{marginTop:8}}>免费注册</Button>
-      </div>
+    return (
+      <>
+        <div className="reg"><InputItem
+          {...getFieldProps('phone')}
+          type="phone"
+          placeholder="请输入手机号"
+        >+86 <Icon type="down" size='xxs' /></InputItem>
+          <InputItem
+            {...getFieldProps('password')}
+            type="password"
+            placeholder="请输入验证码"
+          ></InputItem>
+          <Button type="primary" style={{ borderRadius: 22, marginTop: 16, fontSize: 14 }}>登陆</Button>
+          <p><span>密码登陆</span><span>无法登陆？</span></p>
+        </div>
+        <p style={{ width: "100%",marginTop:80, textAlign: "center", color: "#aaa", fontSize: 12 }}>—— 其他方式登陆 ——</p>
+        <div class='botton'>
+          <div class='Icon' style={{ background: "green" }}>
+            <WechatOutlined />
+          </div>
+          <div class='Icon' style={{ background: "blue" }}>
+            <QqOutlined />
+          </div>
+          <div class='Icon' style={{ background: "red" }}>
+            <WeiboOutlined />
+          </div>
+        </div>
+      </>
     )
   }
 }
