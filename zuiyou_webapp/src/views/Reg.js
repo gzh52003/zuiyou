ad5cedb81ee74acade37c2ca0d6e21b605f36546
liuyiliang07 <851260830@qq.com>
2020-09-27 16:21:35 +0800

    提交mine页面及reg页面修改9/27

--- zuiyou_webapp/src/views/Reg.js
+++ zuiyou_webapp/src/views/Reg.js
@@ -1,42 +1,64 @@
 import React from 'react'
-import { Button, InputItem, Icon } from 'antd-mobile'
+import { Button, InputItem, Icon, Toast } from 'antd-mobile'
 import { WechatOutlined, QqOutlined, WeiboOutlined } from '@ant-design/icons'
 import { createForm } from 'rc-form'
 import "../scss/reg.scss"
 class Reg extends React.Component {
-  state={
-    phoneNum:'',
-    password:''
+  state = {
+    phoneNum: '',
+    password: '',
+    isGetNum: false,
+    num: 60
   }
-  goReg=()=>{
-    console.log(this.state.password);
+  // 走短信验证码倒计时
+  numRun = () => {
+    if (!this.state.isGetNum) {
+      Toast.info('验证码已发送', 2, null, false);
+      this.setState({ isGetNum: true });
+      let time = setInterval(() => {
+        this.setState({ num: this.state.num - 1 })
+        if (this.state.num <= 0) {
+          clearInterval(time);
+          this.setState({ isGetNum: false, num: 60 })
+        }
+      }, 1000)
+    }
   }
-  psdChange=()=>{
+  goReg = () => {
+    console.log(this.state.phoneNum,this.state.password);
+  }
 
-    // console.log(this.inptxt.value);
-    this.setState({password:this.inptxt.value})
-  }
   render() {
     const { getFieldProps } = this.props.form;
     return (
       <div className="regBlock">
-        <div className="reg"><InputItem
-          {...getFieldProps('phone')}
-          type="phone"
-          placeholder="请输入手机号"
-        >+86 <Icon type="down" size='xxs' /></InputItem>
+        <div style={{ padding: 36, background: '#1E90FF' }}>
+          <Icon onClick={() => { this.props.history.goBack() }} type='left' size='s' color="white" />
+        </div>
+        <div className="reg">
+          <InputItem
+            {...getFieldProps('phone')}
+            type="phone"
+            clear
+            onBlur={(e) => {console.log(e,e.trim());;this.setState({phoneNum:e.trim()})}}
+            placeholder="请输入手机号"
+          >+86 <Icon type="down" size='xxs' /></InputItem>
           <InputItem
             {...getFieldProps('password')}
-            type="password"
-            value={this.state.password}
-            ref={(inp)=>{this.inptxt = inp}}
-            onChange={this.psdChange}
+            type="text"
+            onBlur={(e) => {this.setState({password:e})}}
             placeholder="请输入验证码"
+            extra={<span style={{ fontSize: 12, height: 21, width: 80 }}
+              onClick={() => { this.numRun() }}>
+              <span> | </span>
+              {this.state.isGetNum ?
+                <span style={{ width: 60, display: 'inline-block', textAlign: "center", color: '#1E90FF' }}>{this.state.num}</span>
+                : '获取验证码'}</span>}
           ></InputItem>
-          <Button type="primary" onClick={this.goReg} style={{ borderRadius: 22, marginTop: 16, fontSize: 14 }}>注册</Button>
+          <Button type="primary" onClick={this.goReg} style={{ borderRadius: 22, marginTop: 16, fontSize: 14 }}>登陆</Button>
           <p><span>密码登陆</span><span>无法登陆？</span></p>
         </div>
-        <p style={{ width: "100%",marginTop:80, textAlign: "center", color: "#aaa", fontSize: 12 }}>—— 其他方式登陆 ——</p>
+        <p style={{ width: "100%", marginTop: 80, textAlign: "center", color: "#aaa", fontSize: 10 }}>—— 其他方式登陆 ——</p>
         <div className='botton'>
           <div className='Icon' style={{ background: "#00CD66" }}>
             <WechatOutlined />
